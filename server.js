72087500eed708bebccad07eab915c0493c02f90
Andrew Cunningham <wacii@mail.com>
2018-01-12 17:07:15 -0500

    Add apollo server

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
 const next = require("next");
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
+
 const app = next({ dev });
 const handler = app.getRequestHandler();
 
+const fakeUsers = [{ id: "first" }, { id: "second" }];
+const typeDefs = `
+  type Query { users: [User] }
+  type User { id: String }
+`;
+const resolvers = {
+  Query: { users: () => fakeUsers }
+};
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
 app.prepare().then(() => {
   const server = express();
+
+  server.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+  server.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
   server.get("*", handler);
 
